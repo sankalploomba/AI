15308fd69443d58238a8802f639f9f8c3aa67aab
Sankalp Loomba <39914812+sankalploomba@users.noreply.github.com>
2020-12-26 23:17:56 +0530

    Update 4bit_register.pl

--- Digital Circuits/4bit_register.pl
+++ Digital Circuits/4bit_register.pl
@@ -1,6 +1,5 @@
 :-include(flipflops).
 
-
 %% FOUR BIT REGISTER							C:Clock, D0-D3 are inputs for data line, P0-P3 are present states for D flip-flop, Q0-Q4 are updated values stored in register
 reg(C,D0,D1,D2,D3,P0,P1,P2,P3,Q0,Q1,Q2,Q3):-
 	d_ff(C,D0,P0,Q0),
